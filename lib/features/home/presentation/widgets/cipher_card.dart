7d144429ce78d602923c7d12ff8011b1a8786180
Venkatesh Prasad <venkateshprasadvm@gmail.com>
2020-04-22 21:56:37 +0530

    Added Expandable card and Copy to Clipboard ✨

--- lib/features/home/presentation/widgets/cipher_card.dart
+++ lib/features/home/presentation/widgets/cipher_card.dart
@@ -1,4 +1,8 @@
+import 'package:ant_icons/ant_icons.dart';
+import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutterwarden/core/theme/colors.dart';
 
 import '../../domain/entities/sync_entities/cipher.dart';
 
@@ -9,20 +13,84 @@ class CipherCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(
-        horizontal: 24,
-        vertical: 16,
-      ),
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.circular(16),
       ),
-      child: Column(
-        children: <Widget>[
-          Text(cipher.name),
-          Text(cipher.login.username),
-        ],
+      child: ExpandablePanel(
+        header: Column(
+          children: <Widget>[
+            Text(cipher.name),
+            Text(cipher.login.username),
+          ],
+        ),
+        expanded: Container(
+          margin: const EdgeInsets.only(top: 16),
+          child: Row(
+            children: <Widget>[
+              Text(
+                'TAP TO COPY',
+                style: TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black54,
+                ),
+              ),
+              const SizedBox(width: 12),
+              IconButton(
+                icon: Icon(AntIcons.key_outline),
+                onPressed: () => copyToClipboardAndShowScaffold(
+                  context,
+                  cipher.login.password,
+                ),
+              ),
+              IconButton(
+                icon: Icon(AntIcons.user),
+                onPressed: () => copyToClipboardAndShowScaffold(
+                  context,
+                  cipher.login.username,
+                ),
+              ),
+              IconButton(
+                icon: Icon(AntIcons.edit_outline),
+                onPressed: () => null,
+              ),
+              Spacer(),
+              IconButton(
+                icon: Icon(AntIcons.select),
+                onPressed: () => null,
+              ),
+            ],
+          ),
+        ),
+        theme: ExpandableThemeData(
+          hasIcon: false,
+          tapBodyToExpand: true,
+          tapBodyToCollapse: true,
+          tapHeaderToExpand: true,
+          animationDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
+  }
+
+  Future<void> copyToClipboardAndShowScaffold(
+      BuildContext context, String text) async {
+    await Clipboard.setData(ClipboardData(text: text));
+    Scaffold.of(context)
+      ..hideCurrentSnackBar()
+      ..showSnackBar(
+        SnackBar(
+          content: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const <Widget>[
+              Text('Copied to clipboard'),
+              Icon(AntIcons.check_circle),
+            ],
+          ),
+          backgroundColor: Colors.lightBlue,
+        ),
+      );
   }
 }
